2e9310f2b0cb53f9e8ba078f7756ff4a7491a205
HolyMaria01 <113909160+HolyMaria01@users.noreply.github.com>
2022-10-04 17:32:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp ({
+    data () {
+        return {
+            product: 'Socks'
+            image: 'C:\Users\maarj\Documents\Veebidisain\Vue3-shopping\assets\images\socks_blue.jpg'
+        }
+    }
+})
