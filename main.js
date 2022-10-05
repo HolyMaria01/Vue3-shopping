5522fa02e506ae7d2b1f9061525711972c33c44b
Maarja-Liis <maarjaliisuke@gmail.com>
2022-10-05 15:53:36 +0300

    Lesson 2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp ({
     data () {
         return {
             product: 'Socks'
-            image: 'C:\Users\maarj\Documents\Veebidisain\Vue3-shopping\assets\images\socks_blue.jpg'
+            
         }
     }
 })
