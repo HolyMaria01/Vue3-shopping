2409bb942a5eaf5b866f45d9b3575424892e95ff
Maarja-Liis <maarjaliisuke@gmail.com>
2022-10-06 14:49:40 +0300

    Lesson 6

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const app = Vue.createApp ({
     data () {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/image/socks_green.jpg',
             inventory: 100,
+            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variant: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue'},
+                { id: 2234, color: 'green', image: './assets/image/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/image/socks_blue.jpg' },
             ]
 
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart +=1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
